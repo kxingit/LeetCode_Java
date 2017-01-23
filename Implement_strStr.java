36f993beec4f4173c7ecca3a41c6f9d6fca39df8
kxingit <kxin@me.com>
2017-01-23 17:08:01 -0600

    End of work commit

---
+++ Implement_strStr.java
@@ -0,0 +1,39 @@
+/*
+ *  Implement strStr().
+ *
+ *  Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack. 
+ */
+public class Solution {
+  public int strStr(String s, String t) {
+	int n = s.length(), m = t.length();
+	if(m == 0) return 0;
+	for(int i = 0; i < n; i++) {
+	  if(i + m > n) return -1;
+	  for(int j = 0; j < m; j++) {
+		if(s.charAt(i + j) != t.charAt(j)) break;
+		if(j == m - 1) return i;
+	  }
+	}
+	return -1;
+  }
+}
+
+// v2
+public class Solution {
+  public int strStr(String s, String t) {
+	int n = s.length(), m = t.length();
+	if(m == 0) return 0;
+	for(int i = 0; i < n; i++) {
+	  if(i + m > n) return -1;
+	  for(int j = 0; j < m; j++) {
+		if(s.charAt(i + j) != t.charAt(j)) {
+		  break;
+		}
+		if(j == m - 1) {
+		  return i;
+		}
+	  }
+	}
+	return -1;
+  }
+}
