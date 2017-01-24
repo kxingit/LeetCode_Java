21d0eb3ff9792db5aab52136166680a85ca3afd2
kxingit <kxin@me.com>
2017-01-24 12:04:58 -0600

    Some new java codes

---
+++ Merge_Sorted_Array.java
@@ -0,0 +1,24 @@
+/*
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ */
+
+public class Solution {
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+	int i = m - 1, j = n - 1, k = m + n - 1;
+	while(i >= 0 && j >= 0) {
+	  if(nums1[i] > nums2[j]) {
+		nums1[k] = nums1[i];
+		i--;
+	  } else {
+		nums1[k] = nums2[j];
+		j--;
+	  }
+	  k--;
+	}
+	while (j >= 0) {
+	  nums1[k] = nums2[j];
+	  j--;
+	  k--;
+	}
+  }
+}
