3e32e18175d2a9d55b567bcdcef6fdfe74dfbfc6
kxingit <kxin@me.com>
2017-03-27 11:07:52 -0700

    Monday commit.

---
+++ Graph_Valid_Tree.java
@@ -0,0 +1,154 @@
+/*
+Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to check whether these edges make up a valid tree.
+Note: you can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+*/
+// BFS
+public class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        // 9:45 - 9:48
+        HashMap<Integer, HashSet<Integer>> graph = new HashMap();
+        for(int i = 0; i < n; i++) {
+            graph.put(i, new HashSet());
+        }
+        
+        for(int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+        
+        Queue<Integer> q = new LinkedList();
+        HashSet<Integer> visited = new HashSet();
+        q.add(0);
+        visited.add(0);
+        
+        while(q.size() > 0) {
+            int curr = q.poll();
+            Set<Integer> neis = graph.get(curr);
+            for(Integer nei : neis) {
+                if(visited.contains(nei)) return false;
+                visited.add(nei);
+                q.add(nei);
+                graph.get(nei).remove(curr);
+            }
+        }
+        
+        return visited.size() == n;
+    }
+}
+
+// Union-Find
+public class Solution {
+    // 9:07
+    class UnionFind {
+        HashMap<Integer, Integer> map = new HashMap();
+        UnionFind(int n) {
+            for(int i = 0; i < n; i++) {
+                map.put(i, i);
+            }
+        }
+        
+        public int find(int i) {
+            int fa = map.get(i);
+            while(fa != map.get(fa)) {
+                fa = map.get(fa);
+                map.put(i, fa);
+            }
+            return fa;
+        }
+        
+        public void union(int x, int y) {
+            int fax = find(x), fay = find(y);
+            if(fax != fay) {
+                map.put(fax, fay);
+            }
+        }
+    }
+    public boolean validTree(int n, int[][] edges) {
+        UnionFind uf = new UnionFind(n);
+        // int count = 0;
+        for(int[] edge : edges) {
+            if(uf.find(edge[0]) != uf.find(edge[1])) {
+                uf.union(edge[0], edge[1]);
+                // count++;
+            } else {
+                return false;
+            }
+        }
+        return edges.length == n - 1;
+    }
+}
+
+// v3
+public class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        // 10:38 - 10:41
+        HashMap<Integer, HashSet<Integer>> graph = new HashMap();
+        for(int i = 0; i < n; i++) {
+            graph.put(i, new HashSet());
+        }
+        
+        for(int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+        
+        Queue<Integer> q = new LinkedList();
+        HashSet<Integer> visited = new HashSet();
+        q.add(0);
+        visited.add(0);
+        
+        while(q.size() > 0) {
+            int curr = q.poll();
+            Set<Integer> neis = graph.get(curr);
+            for(Integer nei : neis) {
+                if(visited.contains(nei)) return false;
+                visited.add(nei);
+                q.add(nei);
+                graph.get(nei).remove(curr);
+            }
+        }
+        
+        return visited.size() == n;
+    }
+}
+
+// v4
+public class Solution {
+    class UnionFind {
+        HashMap<Integer, Integer> map = new HashMap();
+        
+        UnionFind(int n) {
+            for(int i = 0; i < n; i++) {
+                map.put(i, i);
+            }
+        }
+        
+        public int find(int x) {
+            int fa = map.get(x);
+            while(fa != map.get(fa)) {
+                fa = map.get(fa);
+                map.put(x, fa);
+            }
+            return fa;
+        }
+        
+        public void union(int x, int y) {
+            if(find(x) != find(y)) {
+                map.put(find(x), find(y));
+            }
+        }
+    }
+    public boolean validTree(int n, int[][] edges) {
+        // 10:42 - 10:47
+        UnionFind uf = new UnionFind(n);
+        
+        for(int[] edge : edges) {
+            if(uf.find(edge[0]) == uf.find(edge[1])) {
+                return false;
+            }
+            uf.union(edge[0], edge[1]);
+        }
+        
+        return edges.length == n - 1;
+    }
+}
---
+++ Plus_One_Linked_List.java
@@ -0,0 +1,81 @@
+/*
+Given a non-negative integer represented as non-empty a singly linked list of digits, plus one to the integer.
+
+You may assume the integer do not contain any leading zero, except the number 0 itself.
+
+The digits are stored such that the most significant digit is at the head of the list.
+*/
+public class Solution {
+    public ListNode plusOne(ListNode head) {
+        // 10:49 - 10:58 // bug - 11:02
+        head = reverse(head);
+        ListNode node = head;
+        int curr = node.val + 1;
+        int carry = curr / 10;
+        curr = curr % 10;
+        node.val = curr;
+        node = node.next;
+        while(node != null) {
+            curr = node.val + carry;
+            carry = curr / 10;
+            curr = curr % 10;
+            node.val = curr;
+            node = node.next;
+        }
+        if(carry > 0) {
+            node = head;
+            while(node.next != null) {
+                node = node.next;
+            }
+            node.next = new ListNode(1);
+        }
+        
+        return reverse(head);
+    }
+    
+    public ListNode reverse(ListNode head) {
+        ListNode node = null;
+        while(head != null) {
+            ListNode tmp = head.next;
+            head.next = node;
+            node = head;
+            head = tmp;
+        }
+        return node;
+    }
+}
+
+// v2
+public class Solution {
+    public ListNode plusOne(ListNode head) {
+        // 10:49 - 10:58 // bug - 11:02
+        head = reverse(head);
+        ListNode node = head;
+        ListNode pre = node;
+        int carry = 1;
+        while(node != null) {
+            int curr = node.val + carry;
+            carry = curr / 10;
+            curr = curr % 10;
+            node.val = curr;
+            pre = node;
+            node = node.next;
+        }
+        if(carry > 0) {
+            pre.next = new ListNode(1);
+        }
+        
+        return reverse(head);
+    }
+    
+    public ListNode reverse(ListNode head) {
+        ListNode node = null;
+        while(head != null) {
+            ListNode tmp = head.next;
+            head.next = node;
+            node = head;
+            head = tmp;
+        }
+        return node;
+    }
+}
